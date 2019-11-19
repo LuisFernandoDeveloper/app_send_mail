34341f2959ef5a91bffa8b781f38f15f01764c59
LuisFernandoDeveloper <luifmoreira@gmail.com>
2019-11-19 11:40:47 -0300

    Melhorando o feedback visual

--- processa_envio.php
+++ processa_envio.php
@@ -16,18 +16,22 @@
         private $para = null;
         private $assunto = null;
         private $mensagem = null;
+        public $status = array( 'codigo_status' => null, 'descricao_status' => '');
 
         public function __get($atributo){
             return $this->$atributo;
         }
+
         public function __set($atributo, $valor){
             $this->$atributo = $valor;
         }
-        public function mensagemValida(){
-        if(empty($this->para) || empty($this->assunto) || empty($this->mensagem)){
-            return false;
-        }
-        return true;
+
+        public function mensagemValida() {
+            if(empty($this->para) || empty($this->assunto) || empty($this->mensagem))   {
+                return false;
+            }
+
+            return true;
         }
     }
 
@@ -41,13 +45,14 @@
 
     if(!$mensagem->mensagemValida()){
         echo 'Mensagem não é valida';
-        die();//mata a aplicação neste ponto
+        //die();mata a aplicação neste ponto
+        header('Location: index.php');
     }
 
     $mail = new PHPMailer(true);
     try {
         //Server settings
-        $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $mail->SMTPDebug = false; //2 aparece todo debug           // Enable verbose debug output
         $mail->isSMTP();                                      // Set mailer to use SMTP
         $mail->Host = 'mail.artesdigitaisweb.com.br';  // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
@@ -74,9 +79,59 @@
         $mail->AltBody = 'É necessario utilizar um client que suporte HTML para ter acesso total ao conteúdo dessa mensagem';
 
         $mail->send();
-        echo 'E-mail enviado com sucesso';
+
+        $mensagem->status['codigo_status'] = 1;
+        $mensagem->status['descricao_status'] = 'E-mail enviado com sucesso';
+
     } catch (Exception $e) {
-        echo 'Não foi possível enviar este e-mail! Por favor tente novamente mais tarde.';
-        echo 'Detalhes do erro: ' . $mail->ErrorInfo;
+
+        $mensagem->status['codigo_status'] = 2;
+        $mensagem->status['descricao_status'] = 'Não foi possível enviar este e-mail! Por favor tente novamente mais tarde. Detalhes do erro: ' . $mail->ErrorInfo;
+
+        //alguma logica que armazene o erro para posterior análise por parte do programar
     }
 ?>
+
+<html>
+	<head>
+		<meta charset="utf-8" />
+    	<title>App Mail Send</title>
+
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	</head>
+
+    <body>
+        <div class="container">
+            <div class="py-3 text-center">
+				<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+				<h2>Send Mail</h2>
+				<p class="lead">Seu app de envio de e-mails particular!</p>
+			</div>
+            <div class="row">
+                <div class="col-ms-12">
+
+                    <? if($mensagem->status['codigo_status'] == 1) { ?>
+
+                        <div class="container">
+                            <h1 class="display-4 text-success">Sucesso</h1>
+                            <p><?= $mensagem->status['descricao_status'] ?></p>
+                            <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+                        </div>
+
+                    <? } ?>
+
+                    <? if($mensagem->status['codigo_status'] == 2) { ?>
+
+                        <div class="container">
+                            <h1 class="display-4 text-danger">Ops!</h1>
+                            <p><?= $mensagem->status['descricao_status'] ?></p>
+                            <a href="index.php" class="btn btn-danger btn-lg mt-5 text-white">Voltar</a>
+                        </div>
+
+                    <? } ?>
+
+                </div>
+            </div>
+        </div>
+
